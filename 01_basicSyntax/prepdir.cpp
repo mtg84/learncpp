642a210334470980298bb714e19f1c8ff5f68f11
Maria <formacionjee@gmail.com>
2021-02-07 13:28:56 +0100

    C examples from C essential training

---
+++ 01_basicSyntax/prepdir.cpp
@@ -0,0 +1,15 @@
+/*This is from C Essential training tutorial by Dan Gookin*/
+
+#include <stdio.h>
+
+#define charout(a) putc (a, stdout)
+
+int main()
+{
+    charout('H');
+    charout('i');
+    charout('!');
+    charout('\n');
+    
+    return 0;
+}
